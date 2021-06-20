521b330cd6ce31cc661d236d0d420ba79fd75e31
naoya7081111 <79408902+naoya7081111@users.noreply.github.com>
2021-06-20 23:15:06 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
+
+export default function App() {
+  return (
+    // ChakraProviderでchakra使える
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
